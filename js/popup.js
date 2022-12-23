28f68d786d3b9858562d33a67cd374638b7e15a9
YI-CHEN\sunny <sunny930817@gmail.com>
2022-12-24 00:26:44 +0800

    first

--- js/popup.js
+++ js/popup.js
@@ -7,6 +7,6 @@
 		setTimeout(() => $('.log-2').show(),6000);
 		setTimeout(() => $('.log-3').show(), 9000);
 		setTimeout(() => $('#scoreModal').modal('show'), 12000);
-		setTimeout(() => $('#end-popup').modal('show'), 15000);
+		setTimeout(() => $('#end-popup').modal('show'), 1000);
 	});
 })(jQuery);
